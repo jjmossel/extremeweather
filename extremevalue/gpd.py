69e55976674b6318dbeb14d413f300e9353711f9
jjmossel <jorn.mossel@gmail.com>
2023-01-01 23:17:58 -0500

    add initial cut for gev

--- extremevalue/gpd.py
+++ extremevalue/gpd.py
@@ -50,7 +50,7 @@ class GPDMLE:
         return p * (1 - p) / self.n_sample
 
     def cov(self) -> np.ndarray:
-        fisher_info = hessian(self.neg_ll)(np.array([self.xi, self.sigma]))
+        fisher_info = hessian(self._neg_ll)(np.array([self.xi, self.sigma]))
         cov = np.linalg.inv(fisher_info)
         return cov
 
@@ -137,3 +137,189 @@ class GPDMLE:
 
         plt.hist(self.x_exceed, bins=n, range=(x0, x1), density=True)
         plt.plot(x_vals, pdf)
+
+
+def gev_logp(x, mu, sigma, xi):
+
+    z = (x - mu) / sigma
+    t = 1.0 + xi * z
+
+    return -np.power(t, -1.0 / xi) - (1.0 / xi + 1.0) * np.log(t) - np.log(sigma)
+
+
+def gev_pdf(x, mu, sigma, xi):
+    return np.exp(gev_logp(x, mu, sigma, xi))
+
+
+def gev_returnlevel(rl, mu, sigma, xi):
+    p = 1.0 / rl
+    return mu - (sigma / xi) * (1 - np.power(-np.log(1.0 - p), -xi))
+
+
+def gev_sf(x, mu, sigma, xi):
+    z = (x - mu) / sigma
+    return 1.0 - np.exp(-np.power(1 + xi * z, -1.0 / xi))
+
+
+class GEVMLE:
+    def __init__(self, period=100):
+
+        self.period = period
+        self.x_max = None
+
+        self.mu = None
+        self.sigma = None
+        self.xi = None
+
+    def _fit_x_max(self, x):
+        n_blocks = len(x) // self.period
+        self.x_max = np.max(np.array_split(x, n_blocks), axis=1)
+
+    def _neg_ll(self, theta) -> float:
+        mu, sigma, xi = theta
+        return -1.0 * np.sum(gev_logp(self.x_max, mu, sigma, xi))
+
+    def fit(self, x_data, disp=False):
+
+        self._fit_x_max(x_data)
+
+        neg_xi, mu, sigma = sp.stats.genextreme.fit(self.x_max)
+
+        self.mu = mu
+        self.sigma = sigma
+        self.xi = -neg_xi
+
+    def cov(self) -> np.ndarray:
+        fisher_info = hessian(self._neg_ll)(np.array([self.mu, self.sigma, self.xi]))
+        cov = np.linalg.inv(fisher_info)
+        return cov
+
+    def get_params(self, include_ci=False) -> dict:
+
+        params = {"mu": self.mu, "sigma": self.sigma, "xi": self.xi}
+
+        if include_ci:
+            mu_se, sigma_se, xi_se = np.sqrt(np.diag(self.cov()))
+
+            ci = {
+                "mu": (self.mu - 2 * mu_se, self.mu + 2 * mu_se),
+                "sigma": (self.sigma - 2 * sigma_se, self.sigma + 2 * sigma_se),
+                "xi": (self.xi - 2 * xi_se, self.xi + 2 * xi_se),
+            }
+
+            return (params, ci)
+        else:
+            return params
+
+    def return_level(self, return_period):
+
+        return gev_returnlevel(
+            return_period,
+            self.mu,
+            self.sigma,
+            self.xi,
+        )
+
+    def _return_level_se(self, return_period):
+        def rl(theta):
+            mu, sigma, xi = theta
+            return gev_returnlevel(return_period, mu, sigma, xi)
+
+        delta = grad(rl)(np.array([self.mu, self.sigma, self.xi]))
+
+        cov = self.cov()
+
+        return np.sqrt((delta.dot(cov)).dot(delta))
+
+    def return_level_se(self, return_period):
+
+        return np.vectorize(self._return_level_se)(return_period)
+
+    def _return_periods(self, n=20, log_scale=True):
+
+        n_start = 2
+        n_end = len(self.x_max)
+
+        if log_scale:
+            rl = np.exp(np.linspace(np.log(n_start), np.log(n_end), n))
+        else:
+            rl = np.linspace(n_start, n_end, n)
+        return rl
+
+    def return_level_plot(self, include_ci=False, log_scale=True, period=1.0):
+
+        rp = self._return_periods(n=50, log_scale=log_scale)
+        rl = self.return_level(rp)
+
+        rp_scaled = rp / period
+
+        plt.plot(rp_scaled, rl)
+
+        if include_ci:
+            rl_se = self.return_level_se(rp)
+            plt.fill_between(rp_scaled, rl - 2.0 * rl_se, rl + 2.0 * rl_se, alpha=0.25)
+
+        plt.xlabel("return period")
+        plt.ylabel("return level")
+
+        if log_scale:
+            plt.xscale("log")
+        plt.show()
+
+    def pdf(self, x):
+        return gev_pdf(x, self.mu, self.sigma, self.xi)
+
+    def dist_plot(self, n=20):
+
+        x0 = np.min(self.x_max)
+        x1 = np.quantile(self.x_max, 0.999)
+        x_vals = np.linspace(x0, x1, n)
+
+        pdf = self.pdf(x_vals)
+
+        plt.hist(self.x_max, bins=n, range=(x0, x1), density=True)
+        plt.plot(x_vals, pdf)
+
+    def sf(self, x):
+        return gev_sf(x, self.mu, self.sigma, self.xi)
+
+    def return_period(self, return_level):
+        return 1.0 / self.sf(return_level)
+
+    def _return_period_se(self, return_level):
+        def rp(theta):
+            mu, sigma, xi = theta
+            return 1.0 / gev_sf(return_level, mu, sigma, xi)
+
+        delta = grad(rp)(np.array([self.mu, self.sigma, self.xi]))
+
+        cov = self.cov()
+
+        return np.sqrt((delta.dot(cov)).dot(delta))
+
+    def return_period_se(self, return_level):
+
+        return np.vectorize(self._return_period_se)(return_level)
+
+
+class GEVMLE_ts(GEVMLE):
+    def __init__(self):
+        # only anual for now
+
+        super().__init__(365.25)
+
+    def _fit_x_max(self, ts):
+
+        ts = ts.copy()
+        ts = ts.to_frame("value")
+        ts["year"] = ts.index.year
+
+        def period_max(ts):
+            if ts.count() >= 330:
+                return ts.max()
+            else:
+                return np.nan
+
+        ts_max = ts.groupby("year")["value"].apply(period_max)
+
+        self.x_max = ts_max.dropna().values
